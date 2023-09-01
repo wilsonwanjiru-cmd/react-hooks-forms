506faa0ad5d658027904211ccf27d85fdcf3f188
Wilson Wanjiru <wilsonmuita41@gmail.com>
2023-09-01 11:52:04 +0300

    <Controlled Components>

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Form from './components/Form'
+import ParentComponent from './components/ParentComponent';
 
 ReactDOM.render(
   <Form />,
